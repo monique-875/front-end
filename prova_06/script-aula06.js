01bae8e970fb76c235da7906939074f8746d5aa7
monique-875 <monique.j.fonseca@aluno.senai.br>
2026-03-19 15:48:26 -0300

    organização do codigo

--- prova_06/script-aula06.js
+++ prova_06/script-aula06.js
@@ -1,3 +1,5 @@
+
+// Interação (Clique): Ao clicar no botão "Curtir", incremente o número no contador de curtidas.//
 const botao = document.querySelector("#btn-curtir")
 const contador = document.querySelector("#contador")
 
@@ -8,6 +10,8 @@ botao.addEventListener("click", () => {
     contador.textContent = curtidas
 })
 
+//Monitoramento (Input): Sempre que o usuário digitar no campo de texto, o parágrafo de "Preview" deve mostrar o texto em tempo real.//
+
 const campo = document.querySelector("#campo-texto")
 const preview = document.querySelector("#preview-texto")
 
@@ -15,6 +19,7 @@ campo.addEventListener("input", () => {
     preview.textContent = campo.value
 })
 
+// Sensores (Mouse): Ao entrar com o mouse na caixa de cor, mude-a para azul. Ao sair, ela deve voltar à cor original.//
 
 const caixaCor = document.querySelector("#caixa-cor")
 caixaCor.addEventListener("mouseenter", () => {
@@ -24,6 +29,9 @@ caixaCor.addEventListener("mouseenter", () => {
 caixaCor.addEventListener("mouseleave", () => {
     caixaCor.style.backgroundColor = ""
 })
+
+
+// Desafio Extra (Reset): Crie um botão (ou use uma tecla) que limpe o input e zera o contador ao mesmo tempo.//
 
 const botaoReset = document.querySelector("#btn-reset")
 
